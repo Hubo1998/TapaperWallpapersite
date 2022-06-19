aab52132c810aa9292ce8f590968ac42b4917c4f
Hubert Marchwat <hmarchwat@gmail.com>
2022-06-20 00:58:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Tapaper</title>
+</head>
+
+<body>
+    <header>
+        <div class="topheader">
+            <a href="index.php" class="headerlogo"><img src="images/LogoSample.jpg" alt="Tapaper logo"></a>
+            <a href="Login.php" class="loginbutton">Login</a>
+        </div>
+        <div  class="categoriescontainer">
+            Kategoria
+        </div>
+    </header>
+</body>
+
+</html>
